82cb11bdff75f80ee3b2d21210d02cf9c4251735
terenceneedssurgery <terence@ineedsurgery.com>
2016-11-03 12:19:43 +0000

    Major Update

--- js/ionic.threads.js
+++ js/ionic.threads.js
@@ -6,36 +6,47 @@
         .directive('ionThread', ionThread);
         
 	var threadTemplate = currentScriptPath.replace('js/ionic.threads.js', 'templates/thread.html'),
-		commentTemplate = currentScriptPath.replace('js/ionic.threads.js', 'templates/comment.html');
+		commentTemplate = currentScriptPath.replace('js/ionic.threads.js', 'templates/comment.html'),
+		replyToComment = false;
 
     function ionComment() {
         return {
             restrict: 'EA', // element & attribute
             scope: {
-	            template: '=',
                 comment: '='
             },
-            templateUrl: function(elem, attrs) {
-				return attrs.template || commentTemplate;
-			}
+            templateUrl: commentTemplate,
+            link: function(scope, element, attrs) {
+	            scope.toggleComment = function(comment) {
+		            comment.showChildren = comment.showChildren ? false : true;
+	            },
+	            scope.replyToComment = function(comment) {
+		            replyToComment && replyToComment({comment: comment});
+	            }
+		    }
         }
     }
 
     function ionThread() {
         return {
-            restrict: 'EA', // element & attribute
+            restrict: 'EA', // element & attribute",
             scope: {
+	            commentTemplate: '@',
+	            replyToComment: '&',
 	            template: '=',
                 comments: '='
             },
             templateUrl: function(elem, attrs) {
+	            commentTemplate = attrs.commentTemplate || commentTemplate;
 				return attrs.template || threadTemplate
+				
 			},
-            link: function (scope, element, attrs) {
-	        	scope.commentTemplate = scope.commentTemplate || commentTemplate;
-            }
+			link: function(scope, element, attrs) {
+	            replyToComment = scope.replyToComment || replyToComment;
+		    }
         }
-    }
+    }   
+	
 })(
 	(function () {
         var scripts = document.getElementsByTagName("script");
